332b8911ba4951b674a37c561c38ec34fbbb92a4
vihar <vihar.kurama@gmail.com>
2017-10-02 03:35:45 +0530

    added crawlers

---
+++ pagination.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MultiplequotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        self.log("Visit Successful\n" + response.url)
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract()
+            }
+            yield item
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
